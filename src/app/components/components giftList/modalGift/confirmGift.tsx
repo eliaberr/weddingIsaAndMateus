13f26f464f665aace14f1b603fe03ba68070f348
Eliabr Rodrigues <eliabe859@gmail.com>
2025-11-11 12:35:22 -0300

    fix: fixed the bug deploy and finalized the giftList

--- src/app/components/components giftList/modalGift/confirmGift.tsx
+++ src/app/components/components giftList/modalGift/confirmGift.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "../../ui/ux/button";
 import Cleave from "cleave.js/react";
 
-export default function ConfirmGift({id, setState}: {id :number, setState: () => void}) {
+export default function ConfirmGift({ id, setState }: { id: number; setState: () => void }) {
   const [telefone, setTelefone] = useState("");
 
   const validarTelefone = (numero: string) => {
@@ -10,36 +10,34 @@ export default function ConfirmGift({id, setState}: {id :number, setState: () =>
     return regex.test(numero);
   };
 
-  const handleConfirm = () => {
+  const handleConfirm = async () => {
     if (!validarTelefone(telefone)) {
       alert("Por favor, insira um número de telefone válido!");
       return;
-    } else {
-      alert("Presente confirmado com sucesso!");
-      confirmGift();
     }
-    setState();
+
+    try {
+      const res = await fetch(`https://prj-casamento-backend.vercel.app/presentes/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ phone: telefone }),
+      });
+
+      if (!res.ok) throw new Error("Erro ao confirmar presente");
+
+      const data = await res.json();
+      console.log("Resposta do backend:", data);
+
+      alert("Presente confirmado com sucesso!");
+      setState();
+    } catch (err) {
+      console.error(err);
+      alert("Ocorreu um erro ao confirmar o presente.");
+    }
   };
 
-  function confirmGift() {
-  fetch(`http://localhost:3001/presentes/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      phone: telefone
-    })
-  })
-  .then(res => res.json())
-  .then(data => {
-    console.log("Resposta do backend:", data)
-  })
-  .catch(err => console.error(err))
-}
-
   return (
-    <div className="flex flex-col justify-center gap-3 h-full ">
+    <div className="flex flex-col justify-center gap-3 h-full">
       <label htmlFor="telefone">Digite seu número de telefone</label>
       <Cleave
         options={{
