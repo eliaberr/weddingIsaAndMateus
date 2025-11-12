6de06b04a0a147a70e770186a768239c2a459c7f
eliaberr <eliabe859@gmail.com>
2025-11-12 19:53:19 -0300

    fix: fiex the error in title

--- src/app/(page)/giftList/page.tsx
+++ src/app/(page)/giftList/page.tsx
@@ -7,7 +7,7 @@ export default function PageGiftList() {
       <section className="flex flex-col items-center justify-center mt-8 mb-16 ">
         <div className="text-center px-2 lg:px-0">
           <h2 className="font-medium text-2xl">
-            Escolha o Presnete Ideal para os Noivos
+            Escolha o Presente Ideal para os Noivos
           </h2>
           <span className="text-sm">
             O presente que nunca erra começa com P e termina com ixxx…
