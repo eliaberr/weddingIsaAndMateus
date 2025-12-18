097d71edd9873a7249727fd82d55a703872dd885
eliaberr <eliabe859@gmail.com>
2025-12-18 20:02:04 -0300

    feat: project completed

--- src/app/(page)/wacthGift/page.tsx
+++ src/app/(page)/wacthGift/page.tsx
@@ -27,13 +27,3 @@ export default function WacthGift() {
         </div>
     )
 }
-
-{ /**
-  "id":1,
-      "name":"Geladeira Electrolux",
-      "price":3299,
-      "url":"https://loja.electrolux.com.br/geladeira-electrolux-frost-free-duplex-efficient-com-autosense-cor-inox-look-390l--if43s-/p?idsku=310120866&msclkid=36b0628cfea11f03bfd60b865797b381&utm_source=bing&utm_medium=cpc&utm_campaign=%5BVitrio%5D%20PLA%20%7C%20USC%20%7C%20Top%20Produtos&utm_term=2333713430299282&utm_content=Top%20Produtos",
-      "color":"inox",
-      "phone":null,
-      "urlImage
-*/}
